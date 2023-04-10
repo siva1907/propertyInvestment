b8018aef9197bfb25884fb4dcead980dd3515121
siva1907 <siva10082001@gmail.com>
2023-04-10 10:30:20 +0530

    Structure Of Project Created

---
+++ propertyInvestment/src/main/java/com/feuji/PropertyInvestmentApplication.java
@@ -0,0 +1,13 @@
+package com.feuji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyInvestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyInvestmentApplication.class, args);
+	}
+
+}
---
+++ propertyInvestment/src/test/java/com/feuji/PropertyInvestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feuji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyInvestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
