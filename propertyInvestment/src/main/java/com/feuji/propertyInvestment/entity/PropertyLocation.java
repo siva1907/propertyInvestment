07355652e35a6cf3acd5ddd86b6a17cfb4e6429f
sivakrishnareddy826 <sivakrishnareddy826@gmail.com>
2023-04-10 13:04:49 +0530

    Added PropertyLocation entity class

--- propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/PropertyLocation.java
+++ propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/PropertyLocation.java
@@ -1,6 +1,5 @@
 package com.feuji.propertyInvestment.entity;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
