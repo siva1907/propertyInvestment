37b048794c48cf3f3f94c8c00c9a9335ac0eda2f
MUDDUSETTY GOWTHAM <129718417+GowthamMuddusetty@users.noreply.github.com>
2023-04-10 12:57:07 +0530

    Commit

--- propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/Customer.java
+++ propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/Customer.java
@@ -1,5 +1,44 @@
 package com.feuji.propertyInvestment.entity;
 
-public class Customer {
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="customer")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="customer_id")
+	private int id;
+	
+	@Column(name="customer_name")
+	private String customerName;
+	
+	@Column(name="e_Mail")
+	private String customerMail;
+	
+	@Column(name="phone_no")
+	private long customerPhno;
+	
+	@OneToOne(mappedBy = "customer_location", cascade = CascadeType.ALL)
+	private CustomerLocation customerlocation;
 }
