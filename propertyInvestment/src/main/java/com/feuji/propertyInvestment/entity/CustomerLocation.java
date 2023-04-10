8f778b47300e66640db827ae7e043eb7b2e76353
ShireeshaM <shireeshar07@gmail.com>
2023-04-10 13:12:27 +0530

    added entity CustomerLocation

--- propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/CustomerLocation.java
+++ propertyInvestment/src/main/java/com/feuji/propertyInvestment/entity/CustomerLocation.java
@@ -1,5 +1,54 @@
 package com.feuji.propertyInvestment.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.NonFinal;
+
+@Entity
+@Table(name="customer_location")
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class CustomerLocation {
+	
+	@Id
+	@Column(name="customer_location_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="street")
+	private String street;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="state")
+	private String state;
+	
+	@Column(name="country")
+	private String country;
+	
+	@Column(name="pincode")
+	private String pincode;
+	
+	@Column(name="customer_id")
+	@OneToOne
+	@JoinColumn(name="customer_id")
+	private Customer customerId;
 
 }
